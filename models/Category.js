370c6d3a81a1ed34c5ec3087f896650f143ad998
dratsavong <dratsavong@ymail.com>
2023-01-19 20:46:26 -0500

    passed sequelize to model

--- models/Category.js
+++ models/Category.js
@@ -1,23 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Category extends Model { }
 
 Category.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNullfalse: false,
-    },
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
   }
 );
+
 
 module.exports = Category;
